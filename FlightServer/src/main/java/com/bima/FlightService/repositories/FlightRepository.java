2d5f18afab2f8ff88f44c937c0a9cd1887b51dd2
PT. TODIS KARYA BANGSA <biput98@gmail.com>
2023-02-10 23:22:46 +0700

    add annotation

--- FlightServer/src/main/java/com/bima/FlightService/repositories/FlightRepository.java
+++ FlightServer/src/main/java/com/bima/FlightService/repositories/FlightRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bima.FlightService.Entity.Flight;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlightRepository extends JpaRepository<Flight, Integer>{
 
   @Query(value = "SELECT * FROM Flight WHERE departure_airport_code LIKE %?1%", nativeQuery = true)
